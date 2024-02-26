e6e8f6970d2e7b8536bc703e4f5cdeeb3d0b0895
Thamia-99 <78696837+Thamia-99@users.noreply.github.com>
2024-02-26 17:17:24 +0200

    Added a css,html and js files.

---
+++ p3.js
@@ -0,0 +1,59 @@
+const questions = [
+  {
+    question:
+      "What was the name of the first locomotive to carry both passengers and goods in the world?",
+    answers: [
+      { text: "Steam locomotive", correct: false },
+      { text: "Locomotion no.1", correct: true },
+      { text: "Rocket", correct: false },
+      { text: "Limmat", correct: false },
+    ],
+  },
+  {
+    question: "Who invented the first steam engine?",
+    answers: [
+      { text: "Thomas Savery", correct: true },
+      { text: "George Stephenson", correct: false },
+      { text: "Richard Trevithick", correct: false },
+      { text: "Charles Fox", correct: false },
+    ],
+  },
+  {
+    question: "When did the first steam train touch the South African soil?",
+    answers: [
+      { text: "1880", correct: false },
+      { text: "1853", correct: false },
+      { text: "1860", correct: true },
+      { text: "Early 1900s", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestion + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.answers;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
+
+startQuiz();
